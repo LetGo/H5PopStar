6c6718cf869ccda8daa07a515d1407e204e9aeff
zhaochengxue <zhaochengxue@tuyoogame.com>
2018-07-02 19:30:01 +0800

    `1

---
+++ assets/Script/PlayState.js
@@ -0,0 +1,64 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+
+    },
+
+    start () {
+
+    },
+
+    update (dt) {
+
+    },
+
+    onEnter(){
+
+    },
+
+    creatBlocks(){
+        var max_row = Globle.const.MAX_ROW; //最大行
+        var max_col = Globle.const.MAX_COLUMN;//最大列
+        for(var i = max_row - 1; i > 0; --i){
+            for(var j = 0; j < max_col; ++j){
+                
+                createBlockAtRow(i,j,)
+            }
+        }
+    },
+
+    createBlockAtRow(row,column,color){
+
+    },
+});
